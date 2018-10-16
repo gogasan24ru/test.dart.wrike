f883c8c166d8d6863b3c0f9206b9a7e5e9ebdc94
gogasan24ru <a@gogasan.tk>
2018-10-16 14:58:17 +0300

    1st working example

--- main.dart
+++ main.dart
@@ -1,5 +1,50 @@
+//class CashOut{
+
+	calculate (summ)
+	{
+		var ret={
+			50:0,
+			10:0,
+			5:0,
+			1:0
+		};
+
+		ret.forEach((key,value){
+			//var key=ret.indexOf(item);
+			ret[key]=(summ/key).toInt();
+			summ-=ret[key]*key;
+		});
+		return ret;
+	}
+
+	calculate2 (summ)
+	{
+		var ret={
+			50:0,
+			10:0,
+			5:0,
+			1:0
+		};
+
+		while(summ>0)
+			ret.forEach((key,value){
+				//var key=ret.indexOf(item);
+				if(summ>=key){
+						ret[key]++;
+						summ-=key;
+					}
+				//ret[key]=(summ/key).toInt();
+				//summ-=ret[key]*key;
+			});
+		return ret;
+	}
+
+//}
+
 main () 
 {
-	
-
+	print(calculate2(50));
+	print(calculate2(20));
+	print(calculate2(30));
+	print(calculate2(174));
 }
